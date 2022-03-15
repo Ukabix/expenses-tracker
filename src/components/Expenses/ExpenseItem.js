14be9bf216a346c60dcdeb4074d375ab5789d9f8
Ukabix <54454097+Ukabix@users.noreply.github.com>
2022-03-15 21:04:41 +0100

    useState

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
+  // forces returns array [current stale value of var, updatingFunction]
+  const [title, setTitle] = useState(props.title);
+
+
   const clickedHandler = () => {
-    console.log('Clicked!!!');
+    // pass new value
+    setTitle('clicked');
+    console.log(title); // this does not update on first pass
   };
+
   return (
     <Card className='expense-item'>
       {/* pass props to subcomponent */}
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price'>
           ${props.amount}
         </div>
