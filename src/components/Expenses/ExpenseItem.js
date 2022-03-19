c25218608e471e0529b9de1ddffe0eaa56be6c85
Ukabix <54454097+Ukabix@users.noreply.github.com>
2022-03-19 08:51:41 +0100

    add ExpensesFilter component

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,31 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-  // forces returns array [current stale value of var, updatingFunction]
-  const [title, setTitle] = useState(props.title);
-
-
-  const clickedHandler = () => {
-    // pass new value
-    setTitle('clicked');
-    console.log(title); // this does not update on first pass
-  };
-
   return (
     <Card className='expense-item'>
       {/* pass props to subcomponent */}
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className='expense-item__price'>
           ${props.amount}
         </div>
       </div>
-      {/* special prop to fire eventlistener - put js expression or f ponmiter here {} */}
-      <button onClick={clickedHandler}>Change Title</button>
     </Card>
   );
 }
