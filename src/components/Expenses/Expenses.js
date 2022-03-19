9f42a4e7e00ea219eb8714e2003b0f62e26e567c
Ukabix <54454097+Ukabix@users.noreply.github.com>
2022-03-19 10:56:03 +0100

    outputting conditional content in Expenses.js

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -11,6 +11,27 @@ const Expenses = (props) => {
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
+
+  const filteredExpenses = props.items.filter(
+    (expense) => {
+      return (
+        expense.date.getFullYear().toString() ===
+        filteredYear
+      );
+    }
+  ); // returns bool
+
+  let expensesContent = <p>No expenses found.</p>;
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />));
+  }
+
   return (
     <div>
       <Card className='expenses'>
@@ -18,14 +39,20 @@ const Expenses = (props) => {
           selected={filteredYear}
           onYearChange={filterChangeHandler}
         />
-        {props.items.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        {expensesContent}
+        {/* conditional turnary statement - && method
+        {filteredExpenses.length === 0 && (
+          <p>No expenses found.</p>
+        )}
+        {filteredExpenses.length > 0 &&
+          filteredExpenses.map((expense) => (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          ))} */}
       </Card>
     </div>
   );
