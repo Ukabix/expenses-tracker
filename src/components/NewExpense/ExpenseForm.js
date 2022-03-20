f0b6c3d49fa6863faa59fb75e9ffc66d066f950c
Ukabix <54454097+Ukabix@users.noreply.github.com>
2022-03-20 18:32:10 +0100

    add Chart component

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -107,6 +107,12 @@ const ExpenseForm = (props) => {
       </div>
       <div className='new-expense__actions'>
         <button type='submit'>Add Expense</button>
+        <button
+          type='button'
+          onClick={props.onCancel}
+        >
+          Cancel
+        </button>
       </div>
     </form>
   );
